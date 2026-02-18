44788de29099800f580beb585f8a5e8c0dd2a0b0
Samu Ferenc <fecoo2005@gmail.com>
2026-02-18 18:50:38 +0100

    hibak kijavitasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //integer nevenek elirasa
-    std::cout << '1-100 ertekek duplazasa' // "" helyett '', nincs ;
-    for (int i = 0;) // for ciklus ertekei nincsenek megadva teljesen
+    int *b = new int[N_ELEMENTS]; //integer nevenek elirasa
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // "" helyett '', nincs ; nincs endline
+    for (int i = 1; i <= N_ELEMENTS; i++) // for ciklus ertekei nincsenek megadva teljesen, i 1-tol indul, <=
     {
-        b[i] = i * 2;
+        b[i-1] = i * 2; // i 1-es kezdoerteke miatt ki kell vonni 1 et a tomb indexeleshez
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // for ciklus maximuma hibas
     {
-        std::cout << "Ertek:" // ; es i hianya
+        std::cout << "Ertek: " << b[i]<< std::endl;  // ; es i hianya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // ; helyett ,
+    int atlag = 0; // kezdoertek
+    for (int i = 0; i < N_ELEMENTS; i++) // ; helyett ,
     {
-        atlag += b[i] // nincs ;
+        atlag += b[i]; // nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
