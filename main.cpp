c6e26f9b961578e60c7eb79ed6a291f5a97b5517
Samu Ferenc <fecoo2005@gmail.com>
2026-02-18 18:35:57 +0100

    hibak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //integer nevenek elirasa
+    std::cout << '1-100 ertekek duplazasa' // "" helyett '', nincs ;
+    for (int i = 0;) // for ciklus ertekei nincsenek megadva teljesen
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // ; es i hianya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // ; helyett ,
     {
-        atlag += b[i]
+        atlag += b[i] // nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
